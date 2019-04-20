3767b3221abaa62ee2a0f1366873d3f65b327a8b
atulz <atulz@DESKTOP-URF6LTO>
2019-04-20 14:40:27 +0530

    added servlet initializer

--- src/main/java/www/springboot/com/controller/SpringBootApplicationMain.java
+++ src/main/java/www/springboot/com/controller/SpringBootApplicationMain.java
@@ -3,12 +3,17 @@ package www.springboot.com.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootApplicationMain {
+public class SpringBootApplicationMain extends SpringBootServletInitializer {
 	
 	public static void main(String args[]) {
 		SpringApplication.run(SpringBootApplicationMain.class, args);
 	}
-
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(SpringBootApplicationMain.class);
+	    }
 }
